740246ac7f65f67278beb37c4566d568e8d850ff
tracij1286 <tracij50@gmail.com>
2023-03-26 16:30:56 -0400

    added connection to server with sequelize

--- server.js
+++ server.js
@@ -7,3 +7,31 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const sequelize = require('./app/config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+  
+  app.use(session(sess));
+  
+  const helpers = require('./utils/helpers');
+  const hbs = exphbs.create({ helpers });
+  
+  app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+  app.use(require('./app/controllers'));
+  
+  sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
